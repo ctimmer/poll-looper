e37006194f43ce9d8bf5882047416fa727dadcd9
Curt Timmerman <ctimmer@mtaonline.net>
2022-07-25 19:55:14 -0800

    Add files via upload
    Added support for ESP8266

--- examples/pi_watchdog.py
+++ examples/pi_watchdog.py
@@ -13,7 +13,10 @@ class Watchdog:
                  wd_timeout_ms = WATCHDOG_TIMEOUT_MS) :
         #print ("Watchdog: init")
         self.poller = poller
-        self.wdt = machine.WDT (timeout=wd_timeout_ms)
+        try :
+            self.wdt = machine.WDT (timeout=wd_timeout_ms)
+        except :
+            self.wdt = machine.WDT ()    # esp8266?
 
     def poll_it (self) :
         #print ("Watchdog: poll_it")
