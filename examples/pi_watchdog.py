dd92713ff57f3d8cc08b61c41fe779c1c4250458
Curt Timmerman <ctimmer@mtaonline.net>
2022-08-06 10:27:34 -0800

    more readme updates

--- examples/pi_watchdog.py
+++ examples/pi_watchdog.py
@@ -4,7 +4,7 @@
 
 import machine
 
-WATCHDOG_TIMEOUT_MS = 2000
+WATCHDOG_TIMEOUT_MS = 2000              # 2 seconds
 
 class Watchdog:
 
@@ -12,15 +12,15 @@ class Watchdog:
                  poller ,
                  wd_timeout_ms = WATCHDOG_TIMEOUT_MS) :
         #print ("Watchdog: init")
-        self.poller = poller
+        self.poller = poller            # not used, could be omitted
         try :
             self.wdt = machine.WDT (timeout=wd_timeout_ms)
         except :
-            self.wdt = machine.WDT ()    # esp8266?
+            self.wdt = machine.WDT ()   # esp8266?
 
     def poll_it (self) :
         #print ("Watchdog: poll_it")
-        self.wdt.feed ()           # Still going
+        self.wdt.feed ()                # Still going
 
     def shutdown (self) :
         pass
